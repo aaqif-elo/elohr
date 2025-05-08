a7998493a9fe62e9514e7fa1976da7d222d66ac7
aaqif_elo <aaqif@elobyte.com>
2025-05-08 01:54:29 +0100

    feat: restore Discord command enums
    - Reintroduced enums for attendance, admin, leave, slash command options, and authentication commands.
    - This change enhances the structure and organization of command-related constants for better maintainability.

---
+++ src/server/services/discord/discord.enums.ts
@@ -0,0 +1,31 @@
+export enum EAttendanceCommands {
+  BREAK = "break",
+  RESUME = "back",
+  LOGIN = "available",
+  LOGOUT = "oftd",
+  SWITCH = "switch",
+}
+
+export enum EAdminCommands {
+  GET_HOLIDAY_ANNOUNCEMENT = "get-holiday-announcement",
+  OVERRIDE_HOLIDAY_ANNOUNCEMENT = "override-holiday-announcement",
+  ANNOUNCE_NEXT_HOLIDAY = "announce-next-holiday",
+}
+
+export enum ELeaveCommands {
+  REQUEST_LEAVE = "request-leave",
+}
+export enum ESlashCommandOptionNames {
+  BREAK_REASON = "reason",
+  REPORT_DAYS = "days",
+  REPORT_USER = "user",
+}
+
+export enum EAuthCommands {
+  HR = "hr",
+}
+
+export enum EAutoBreakReason {
+  AFK = "AFK",
+  NO_VOICE_CHANNEL = "No Voice Channel",
+}
